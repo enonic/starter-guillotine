34697f39b46becdda4fa55aab2af3357af35a0fc
Christian Westgaard <cwe@enonic.com>
2018-10-10 09:40:37 +0200

    Outcommented CORS example

--- src/main/resources/services/graphql/graphql.js
+++ src/main/resources/services/graphql/graphql.js
@@ -1,13 +1,44 @@
+//──────────────────────────────────────────────────────────────────────────────
+// Imports
+//──────────────────────────────────────────────────────────────────────────────
 var guillotineLib = require('/lib/guillotine');
 var graphQlLib = require('/lib/graphql');
 
-var schema = guillotineLib.createSchema();
+
+//──────────────────────────────────────────────────────────────────────────────
+// Imported functions
+//──────────────────────────────────────────────────────────────────────────────
+var execute = graphQlLib.execute;
+
+
+//──────────────────────────────────────────────────────────────────────────────
+// Constants
+//──────────────────────────────────────────────────────────────────────────────
+/*var CORS_HEADERS = {
+	'Access-Control-Allow-Headers': 'Content-Type',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Origin': 'https://example.com'
+};*/
+
+var SCHEMA = guillotineLib.createSchema();
+
+
+//──────────────────────────────────────────────────────────────────────────────
+// Public functions
+//──────────────────────────────────────────────────────────────────────────────
+/*exports.options = function () {
+	return {
+		contentType: 'text/plain;charset=utf-8',
+		headers: CORS_HEADERS
+	};
+};*/
+
 
 exports.post = function (req) {
- var body = JSON.parse(req.body);
- var result = graphQlLib.execute(schema, body.query, body.variables);
- return {
-     contentType: 'application/json',
-     body: result
- };
+	var body = JSON.parse(req.body);
+	return {
+		contentType: 'application/json',
+		body: execute(SCHEMA, body.query, body.variables)/*,
+		headers: CORS_HEADERS*/
+	};
 };
