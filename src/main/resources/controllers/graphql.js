7a9a92b0c58d6a095fe37376f7d2fb5319f10d60
Thomas Sigdestad <tsi@enonic.com>
2019-07-04 15:27:22 +0200

    Activated CORS headers to allow any origin

--- src/main/resources/controllers/graphql.js
+++ src/main/resources/controllers/graphql.js
@@ -14,11 +14,11 @@ var execute = graphQlLib.execute;
 //──────────────────────────────────────────────────────────────────────────────
 // Constants
 //──────────────────────────────────────────────────────────────────────────────
-/*var CORS_HEADERS = {
+var CORS_HEADERS = {
 	'Access-Control-Allow-Headers': 'Content-Type',
 	'Access-Control-Allow-Methods': 'POST, OPTIONS',
-	'Access-Control-Allow-Origin': 'https://example.com'
-};*/
+	'Access-Control-Allow-Origin': *
+};
 
 var SCHEMA = guillotineLib.createSchema();
 
@@ -26,19 +26,19 @@ var SCHEMA = guillotineLib.createSchema();
 //──────────────────────────────────────────────────────────────────────────────
 // Public functions
 //──────────────────────────────────────────────────────────────────────────────
-/*exports.options = function () {
+exports.options = function () {
 	return {
 		contentType: 'text/plain;charset=utf-8',
 		headers: CORS_HEADERS
 	};
-};*/
+};
 
 
 exports.post = function (req) {
 	var body = JSON.parse(req.body);
 	return {
 		contentType: 'application/json',
-		body: execute(SCHEMA, body.query, body.variables)/*,
-		headers: CORS_HEADERS*/
+		body: execute(SCHEMA, body.query, body.variables),
+		headers: CORS_HEADERS
 	};
 };
