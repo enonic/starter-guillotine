95e1d7a86fbee56b9c6428bd7f6c39bced06aaff
Anatol Sialitski <asi@enonic.com>
2021-03-23 12:19:41 +0300

    Upgrade to new Guillotine 5.0 #12

--- src/main/resources/controllers/graphql.js
+++ src/main/resources/controllers/graphql.js
@@ -1,22 +1,18 @@
 var guillotineLib = require('/lib/guillotine');
 var graphqlPlaygroundLib = require('/lib/graphql-playground');
 var graphQlLib = require('/lib/graphql');
-var graphQlRxLib = require('/lib/graphql-rx');
 var authLib = require('/lib/xp/auth');
-var webSocketLib = require('/lib/xp/websocket');
-
 
 //──────────────────────────────────────────────────────────────────────────────
 // Constants
 //──────────────────────────────────────────────────────────────────────────────
 var CORS_HEADERS = {
-	'Access-Control-Allow-Headers': 'Content-Type',
-	'Access-Control-Allow-Methods': 'POST, OPTIONS',
-	'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Origin': '*'
 };
 
 var SCHEMA = guillotineLib.createSchema();
-
 
 //──────────────────────────────────────────────────────────────────────────────
 // Authentication
@@ -28,53 +24,6 @@ function isAuthenticated() {
 function canAccessAdminLogin() {
     return authLib.hasRole('system.admin') || authLib.hasRole('system.admin.login')
 }
-
-
-
-//──────────────────────────────────────────────────────────────────────────────
-// GraphQL WS Protocol
-//──────────────────────────────────────────────────────────────────────────────
-const graphQlSubscribers = {};
-
-function cancelSubscription(sessionId) {
-    Java.synchronized(() => {
-        const subscriber = graphQlSubscribers[sessionId];
-        if (subscriber) {
-            delete  graphQlSubscribers[sessionId];
-            subscriber.cancelSubscription();
-        }
-    }, graphQlSubscribers)();
-}
-
-function handleStartMessage(sessionId, message) {
-    const graphlqlOperationId = message.id;
-    const payload = message.payload;
-
-    try {
-        const result = graphQlLib.execute(SCHEMA, payload.query, payload.variables);
-
-        if (result.data instanceof com.enonic.lib.graphql.rx.Publisher) {
-            const subscriber = graphQlRxLib.createSubscriber({
-                onNext: (result) => {
-                    webSocketLib.send(sessionId, JSON.stringify({
-                        type: 'data',
-                        id: graphlqlOperationId,
-                        payload: result
-                    }));
-                }
-            });
-            Java.synchronized(() => graphQlSubscribers[sessionId] = subscriber, graphQlSubscribers)();
-            result.data.subscribe(subscriber);
-        }
-    } catch (e) {
-        log.error('Error while handling Start GraphQL-WS message', e);
-        throw e;
-    }
-}
-
-
-
-
 
 //──────────────────────────────────────────────────────────────────────────────
 // Error handling
@@ -107,8 +56,6 @@ function createUnauthorizedError() {
     }
 }
 
-
-
 function createForbiddenError() {
     return {
         status: 403,
@@ -123,15 +70,14 @@ function createForbiddenError() {
     }
 }
 
-
 //──────────────────────────────────────────────────────────────────────────────
 // Methods
 //──────────────────────────────────────────────────────────────────────────────
 exports.options = function () {
-	return {
-		contentType: 'text/plain;charset=utf-8',
-		headers: CORS_HEADERS
-	};
+    return {
+        contentType: 'text/plain;charset=utf-8',
+        headers: CORS_HEADERS
+    };
 };
 
 exports.get = function (req) {
@@ -160,37 +106,11 @@ exports.post = function (req) {
     var body = JSON.parse(req.body);
     return {
         contentType: 'application/json',
-		headers: CORS_HEADERS,
+        headers: CORS_HEADERS,
         body: JSON.stringify(graphQlLib.execute(SCHEMA, body.query, body.variables))
     };
 };
 
-
-exports.webSocketEvent = function (event) {
-    switch (event.type) {
-    case 'close':
-        cancelSubscription(event.session.id);
-        break;
-    case 'message':
-        var message = JSON.parse(event.message);
-        switch (message.type) {
-        case 'connection_init':
-            webSocketLib.send(event.session.id, JSON.stringify({
-                type: 'connection_ack'
-            }));
-            break;
-        case 'start':
-            handleStartMessage(event.session.id, message);
-            break;
-        case 'stop':
-            cancelSubscription(event.session.id);
-            break;
-        }
-        break;
-    case 'error':
-        log.warning('Session [' + event.session.id + '] error: ' + event.error);
-        break;
-    }
-};
+exports.webSocketEvent = guillotineLib.initWebSockets(SCHEMA);
 
 
